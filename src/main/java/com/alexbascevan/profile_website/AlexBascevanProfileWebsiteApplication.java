866d7c9efae357081c6d6ca4cf26870f157dd2de
Alex Bascevan <alexbascevan@icloud.com>
2024-09-10 12:12:27 -0400

    Initial commit

---
+++ src/main/java/com/alexbascevan/profile_website/AlexBascevanProfileWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.alexbascevan.profile_website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlexBascevanProfileWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlexBascevanProfileWebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alexbascevan/profile_website/AlexBascevanProfileWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexbascevan.profile_website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlexBascevanProfileWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
