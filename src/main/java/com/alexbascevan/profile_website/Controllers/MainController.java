92476a7df70aab187f3e54904ed5a48335d51e22
Alex Bascevan <alexbascevan@icloud.com>
2024-09-10 20:25:06 -0400

    Changed index styling

--- src/main/java/com/alexbascevan/profile_website/Controllers/MainController.java
+++ src/main/java/com/alexbascevan/profile_website/Controllers/MainController.java
@@ -19,34 +19,20 @@ public class MainController {
     
     @RequestMapping("/") // Maps the root URL to this method
     public String index(Model model){
-        return "index"; // Returns the name of the view template to be rendered
-    }
-
-    @RequestMapping("/projects") // Maps the projects URL to this method
-    public String projects(Model model){
-        return "projects"; // Returns the name of the view template to be rendered
-    }
-
-    @RequestMapping("/contact") // Maps the contact URL to this method
-    public String contact(Model model){
-
         model.addAttribute("email", new Email()); // Attach an email to the model
-
-        return "contact"; // Returns the name of the view template to be rendered
+        return "index"; // Returns the name of the view template to be rendered
     }
 
     @PostMapping("/sendEmail")
     public String sendEmail(@ModelAttribute Email email, RedirectAttributes redirectAttributes) {
 
-        email.setSubject(email.getSubject() + " : *** From your profile Website ***");
-
+        email.setSubject("*** From your profile Website ***");
         email.setMessage("Senders Name: " + email.getSendersName() + "\nSenders Email: " + email.getSendersEmail() + "\n\n" + email.getMessage());
-
         mailer.sendEmail(email);
 
         // Add a success message and redirect to contact page
         redirectAttributes.addFlashAttribute("message", "Email has been sent successfully!");
-        return "redirect:/contact";
+        return "redirect:/";
     }
     
 }
