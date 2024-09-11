a1d82db4a95f9866c07cb1b9a6b86c70ec4da361
Alex Bascevan <alexbascevan@icloud.com>
2024-09-11 14:50:03 -0400

    Changed Favicon, added in button to download resume

--- frontend/src/components/IntroSection/IntroSection.js
+++ frontend/src/components/IntroSection/IntroSection.js
@@ -13,13 +13,16 @@ const IntroSection = () => {
   return (
     <section id="intro" className="section intro">
         <div className="intro-content">
+            <p className="hi" >Hi, my name is</p>
             <h1 className="name">Alexander Bascevan</h1>
             <h3 className="title">Software Engineer | Cybersecurity Enthusiast</h3>
             <p className="summary">
-              I’m a Software Engineer based in the Greater Toronto Area, specializing in creating exceptional digital 
+              I’m a <span style={{ color: '#00f9e3' }}>Software Engineer </span>
+              based in the Greater Toronto Area, specializing in creating exceptional digital 
               experiences. With a keen interest in cybersecurity, I am dedicated to building robust, secure solutions 
               that meet the highest standards.
             </p>
+            <br/>
             <button className="contact-button" onClick={scrollToContact}>Get In Touch</button>
         </div>
         <div className="intro-image-wrapper">
--- frontend/src/components/TopNavBar/NavBar.js
+++ frontend/src/components/TopNavBar/NavBar.js
@@ -1,6 +1,5 @@
-// src/components/NavBar.js
 import React from 'react';
-import './NavBar.css'; // Create this CSS file for styling
+import './NavBar.css'; 
 
 const NavBar = () => {
   const scrollToSection = (id) => {
@@ -10,13 +9,33 @@ const NavBar = () => {
     }
   };
 
+  const handleResumeDownload = () => {
+    const link = document.createElement('a');
+    link.href = '/resume/resume.pdf'; // Path to your PDF file
+    link.download = 'resume.pdf'; // Filename for the downloaded file
+    link.click(); // Trigger the download
+  };
+
   return (
     <nav className="navbar">
-      <button onClick={() => scrollToSection('intro')} className="nav-button">Intro</button>
-      <button onClick={() => scrollToSection('background')} className="nav-button">About Me</button>
-      <button onClick={() => scrollToSection('projects')} className="nav-button">Projects</button>
-      <button onClick={() => scrollToSection('technologies')} className="nav-button">Technologies</button>
-      <button onClick={() => scrollToSection('contact')} className="nav-button">Contact</button>
+      <button onClick={() => scrollToSection('intro')} className="nav-button">
+        <span style={{ color: '#00f9e3' }}>01. </span>Intro
+      </button>
+      <button onClick={() => scrollToSection('background')} className="nav-button">
+        <span style={{ color: '#00f9e3' }}>02. </span>About Me
+      </button>
+      <button onClick={() => scrollToSection('projects')} className="nav-button">
+        <span style={{ color: '#00f9e3' }}>03. </span>Projects
+      </button>
+      <button onClick={() => scrollToSection('technologies')} className="nav-button">
+        <span style={{ color: '#00f9e3' }}>04. </span>Technologies
+      </button>
+      <button onClick={() => scrollToSection('contact')} className="nav-button">
+        <span style={{ color: '#00f9e3' }}>05. </span>Contact
+      </button>
+      <button onClick={handleResumeDownload} className="resume-button">
+        <span style={{ color: '#00f9e3' }}>Resume</span>
+      </button>
     </nav>
   );
 };
