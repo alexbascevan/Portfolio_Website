8c23db9ed6c9b0efc7998509f63d8ab422fc19b9
Alex Bascevan <alexbascevan@icloud.com>
2024-09-11 20:12:33 -0400

    added in a start for my about me section, needs improvment

--- frontend/src/components/MyBackgroundSection/MyBackgroundSection.js
+++ frontend/src/components/MyBackgroundSection/MyBackgroundSection.js
@@ -1,17 +1,32 @@
 // src/components/MyBackgroundSection.js
 import React from 'react';
-import './MyBackgroundSection.css'; // Create this CSS file to style this component
+import './MyBackgroundSection.css'; // Ensure this CSS file is created for styling
 
 const MyBackgroundSection = () => {
   return (
-    <section id="background" class="section myBackground">
-    <div>
-        <h1>My Background</h1>
-        <h2>Education:</h2>
-        <p>Details about your education...</p>
-        <h2>Technologies</h2>
-    </div>
-</section>
+    <section id="background" className="myBackground">
+      <div className="container">
+        <h1 style={{ color: '#00f9e3' }}>My Background</h1>
+        
+        <h2 style={{ color: '#00f9e3' }}>Education</h2>
+        <p>
+          I am currently in my final year of an Associate’s Degree in Software Development and Network Engineering at Sheridan College. The program offers a challenging curriculum with instruction in leading-edge computer systems technologies including Cloud, Big Data, Spring Boot, Full-Stack Web Development, Android Kotlin, iOS Swift, and Unity.
+          <br /><br />
+          Sheridan's program also emphasizes practical skills with a capstone project and opportunities for further studies, including bachelor's degrees and university transfers.
+        </p>
+        
+        <h2 style={{ color: '#00f9e3' }}>Certifications</h2>
+        <p>
+          I have completed several professional certificates, including:
+          <ul>
+            <li>Google Careers Professional Certificate in Cybersecurity</li>
+            <li>Docker Foundations Professional Certificate</li>
+            <li>GitHub Career Essentials Professional Certificate</li>
+          </ul>
+          My academic journey and certifications have provided me with a solid foundation in software development and cybersecurity, preparing me for various technical roles and challenges.
+        </p>
+      </div>
+    </section>
   );
 };
 
